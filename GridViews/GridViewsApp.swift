55ab96bc126a030e60cc4acd647cdc079a0e9eeb
Sandy Gomez <sgomez1029@gmail.com>
2023-06-15 14:49:55 -0700

    Initial Commit

---
+++ GridViews/GridViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewsApp.swift
+//  GridViews
+//
+//  Created by Sandra Gomez on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GridViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
