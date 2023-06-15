9c70568f1d7d4c1beea9c712cd6ddd52b6dbaaec
Sandy Gomez <sgomez1029@gmail.com>
2023-06-15 16:40:45 -0700

    GridViews

--- GridViews/ContentView.swift
+++ GridViews/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    var symbols = ["headphones"]
+    var colors : [Color] = [.gray]
+    var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItemLayout, spacing: 20) {
+                    ForEach((0...999), id: \.self){
+                        Image(systemName: symbols[$0 % symbols.count])
+                            .font(.system(size: 30))
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                            .background(colors[$0 % colors.count])
+                            .cornerRadius(10)
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Events")
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
